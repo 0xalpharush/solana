firedancer-io/agave

avoid sysvar instructions panic

transaction-context/src/lib.rs
@@ -401,10 +401,12 @@ impl TransactionContext {
                 .ok_or(InstructionError::NotEnoughAccountKeys)?
                 .try_borrow_mut()
                 .map_err(|_| InstructionError::AccountBorrowFailed)?;
-            instructions::store_current_index(
-                mut_account_ref.data_as_mut_slice(),
-                self.top_level_instruction_index as u16,
-            );
+            if mut_account_ref.data().len() >= 2 {
+                instructions::store_current_index(
+                    mut_account_ref.data_as_mut_slice(),
+                    self.top_level_instruction_index as u16,
+                );
+            }
         }
         Ok(())
     }

0xalpharush/solana

transaction-context/src/lib.rs
@@ -429,10 +429,12 @@ impl TransactionContext {
             if mut_account_ref.owner() != &solana_sdk_ids::sysvar::id() {
                 return Err(InstructionError::InvalidAccountOwner);
             }
-            instructions::store_current_index_checked(
-                mut_account_ref.data_as_mut_slice(),
-                self.top_level_instruction_index as u16,
-            )?;
+            if mut_account_ref.data().len() >= 2 {
+                instructions::store_current_index_checked(
+                    mut_account_ref.data_as_mut_slice(),
+                    self.top_level_instruction_index as u16,
+                )?;
+            }
         }
         Ok(())
     }